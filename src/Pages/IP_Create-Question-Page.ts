479fd5f4ae9c58fa3047f738f533ad26bf2c47b1
jithin.somaraj@innovaturelabs.org <jithin.somaraj@innovaturelabs.org>
2023-09-29 10:40:44 +0530

    push

--- src/Pages/IP_Create-Question-Page.ts
+++ src/Pages/IP_Create-Question-Page.ts
@@ -171,7 +171,7 @@ public async exclusivetogglebutton(){
   //await this.exclusivetoggle.click();
 }
 
-// It is published with issue 3 thengaa fail
+// It is published with issue 3 thengaa fail225
 
 
 
